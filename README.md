a4577a740e59c82325e0984b820f0e9677e4c64d
Egor Konyk <e.konyk1@gmail.com>
2022-08-13 14:01:16 +0500

    Add more cases for search benchmark and add results to README

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
-# taxi_matcher
+# Taxi Driver Matcher
  
-A REST API for matching taxi drivers with pedestrians.
+A Golang implementation of REST API service for matching taxi drivers with pedestrians. QuadTree data structure for matching was used. The packages are covered with tests by **99%**
+
+-------------------
+
+### Driver searching benchmark
+The positions of the driver and pedestrian are randomly distributed on the map.
+
+```
+BenchmarkSearch/100_drivers_100000_mapdimension-8         	  861868	      1329 ns/op	      72 B/op	       3 allocs/op
+BenchmarkSearch/1000_drivers_100000_mapdimension-8        	  767870	      1554 ns/op	      75 B/op	       3 allocs/op
+BenchmarkSearch/10_drivers_1000000_mapdimension-8         	 1000000	      1247 ns/op	      61 B/op	       2 allocs/op
+BenchmarkSearch/1000000_drivers_1000_mapdimension-8       	  672942	      1694 ns/op	     156 B/op	       6 allocs/op
+``
--- internal/service/benchmark_test.go
+++ internal/service/benchmark_test.go
@@ -1,28 +1,36 @@
 package service
 
 import (
+	"fmt"
 	"math/rand"
 	"testing"
 )
 
 func BenchmarkSearch(b *testing.B) {
-	b.StopTimer()
-
-	dim := 100000
-	service := NewService(dim)
-
-	for i := 0; i < 1e4; i++ {
-		service.Add(&Driver{Id: i, X: rand.Intn(dim), Y: rand.Intn(dim)})
+	cases := []struct{ drivers, dim int }{
+		{1e2, 1e5}, {1e3, 1e5}, {1e1, 1e6}, {1e6, 1e3},
 	}
 
-	queries := make([]*Pedestrian, b.N)
-	for i := 0; i < b.N; i++ {
-		queries[i] = &Pedestrian{X: rand.Intn(dim), Y: rand.Intn(dim)}
+	for _, c := range cases {
+		b.Run(fmt.Sprintf("%d_drivers_%d_mapdimension", c.drivers, c.dim), func(b *testing.B) {
+			b.StopTimer()
+			service := NewService(c.dim)
+
+			for i := 0; i < c.drivers; i++ {
+				service.Add(&Driver{Id: i, X: rand.Intn(c.dim), Y: rand.Intn(c.dim)})
+			}
+
+			queries := make([]*Pedestrian, b.N)
+			for i := 0; i < b.N; i++ {
+				queries[i] = &Pedestrian{X: rand.Intn(c.dim), Y: rand.Intn(c.dim)}
+			}
+
+			b.StartTimer()
+
+			for i := 0; i < b.N; i++ {
+				service.SearchClosest(queries[i])
+			}
+		})
 	}
 
-	b.StartTimer()
-
-	for i := 0; i < b.N; i++ {
-		service.SearchClosest(queries[i])
-	}
 }
